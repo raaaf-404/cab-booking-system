20998e230678ac1107c42f847b03a20581ef06c3
raaaf-404 <rafaeltuan404@gmail.com>
2025-06-03 03:06:13 +0800

    added created_by and last_modified_by in  User entity class

--- cab-booking-backend/src/main/java/com/cabbooking/model/User.java
+++ cab-booking-backend/src/main/java/com/cabbooking/model/User.java
@@ -8,8 +8,14 @@
     
     import org.hibernate.annotations.CreationTimestamp;
     import org.hibernate.annotations.UpdateTimestamp;
+
     
     import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+    import org.springframework.data.annotation.CreatedDate;
+    import  org.springframework.data.annotation.LastModifiedDate;
+    import org.springframework.data.annotation.CreatedBy;
+    import org.springframework.data.annotation.LastModifiedBy;
+
     
     import com.fasterxml.jackson.annotation.JsonIgnore;
     
@@ -63,11 +69,23 @@
             @Column(name = "role")
             private Set<String> roles = new HashSet<>();
 
+
+        @CreatedBy
+        @Column(name = "created_by", updatable = false)
+        private String createdBy;
+
         @CreationTimestamp
+        @CreatedDate
         @Column(name = "created_at", updatable = false)
         private LocalDateTime createdAt;
 
+        @LastModifiedBy
+        @Column(name = "last_modified_by", updatable = false)
+        private String lastModifiedBy;
+        
+
         @UpdateTimestamp
+        @LastModifiedDate
         @Column(name = "updated_at")
         private LocalDateTime updatedAt;
 
