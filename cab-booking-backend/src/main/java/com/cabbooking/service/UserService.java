c5aba5982a39b3c97378ac6fa87afab7d154e7ab
raaaf-404 <rafaeltuan404@gmail.com>
2025-06-07 10:40:16 +0800

    added user service interface

---
+++ cab-booking-backend/src/main/java/com/cabbooking/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cabbooking.service;
+
+import com.cabbooking.dto.request.UserRegistrationRequest;
+import com.cabbooking.dto.response.UserResponse;
+import com.cabbooking.model.User;
+import java.util.Optional;
+
+
+public interface UserService {
+    UserResponse registerUser(UserRegistrationRequest registrationRequest);
+    Optional<UserResponse> getUserById(Long userId);
+    Optional<User> findByEmail(String email);
+    
+}
