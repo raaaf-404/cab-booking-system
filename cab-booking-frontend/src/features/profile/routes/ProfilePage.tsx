30136a479710e86645af7e11f9798f73467ac20f
raaaf-404 <rafaeltuan404@gmail.com>
2025-11-10 21:30:22 +0800

    added ProfilePage component

---
+++ cab-booking-frontend/src/features/profile/routes/ProfilePage.tsx
@@ -0,0 +1,10 @@
+const ProfilePage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-purple-600">Profile Page</h1>
+      <p>This is where the user's profile and ride history will go.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
