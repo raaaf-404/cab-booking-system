53ec9d0eeae5270ecf3f191f69d3467fbaef7f89
raaaf-404 <rafaeltuan404@gmail.com>
2026-02-17 10:30:58 +0800

    refactor api.ts: made User type as interface

--- cab-booking-frontend/src/types/api.ts
+++ cab-booking-frontend/src/types/api.ts
@@ -1,9 +1,34 @@
+// ==========================================
+// 1. CORE DOMAIN MODELS (The "Things")
+// ==========================================
 // Corresponds to UserResponse.java
-export type User = {
-  id: number; // Java Long is best represented as number in TS
-  username: string;
+export interface User {
+  id: number;
   email: string;
   roles: string[];
+}
+
+
+// Corresponds to CabResponse.java
+export type Cab = {
+    cabId: number;
+    cabType: string;
+    licensePlate: string;
+    driver: User; //User type is from UserResponse
+    available: boolean;
+    currenLocation: string;
+};
+
+export type Booking = {
+    bookingId: number;
+    user: User;
+    cab: Cab;
+    pickupLocation: string;
+    dropoffLocation: string;
+    pickupTime: string;
+    dropOffTime: string;
+    bookingStatus: string;
+    fare: number;
 };
 
 // Corresponds to LoginRequest.java
@@ -11,7 +36,6 @@ export type LoginRequest = {
   email: string;
   password: string;
 };
-
 // Corresponds to SignupRequest.java
 export type SignupRequest = {
   name: string;
@@ -20,8 +44,6 @@ export type SignupRequest = {
   phone: string;
   roles?: string[]; // We make this optional on the frontend
 };
-
-
 // Corresponds to MessageResponse.java
 export type MessageResponse = {
   message: string;
@@ -35,28 +57,6 @@ export type TokenRefreshResponse = {
     accessToken: string;
     refreshToken: string;
     tokenType: string;  // e.g., "Bearer"
-};
-
-// Corresponds to CabResponse.java
-export type Cab = {
-  cabId: number;
-  cabType: string;
-  licensePlate: string;
-  driver: User; //User type is from UserResponse
-  available: boolean;
-  currenLocation: string;
-};
-
-export type Booking = {
-  bookingId: number;
-  user: User;
-  cab: Cab;
-  pickupLocation: string;
-  dropoffLocation: string;
-  pickupTime: string;
-  dropOffTime: string;
-  bookingStatus: string;
-  fare: number;
 };
 
 
