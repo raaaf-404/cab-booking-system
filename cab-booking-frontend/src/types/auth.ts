989b27c4833fdd16d27e1625e505b6fe73f47a98
raaaf-404 <rafaeltuan404@gmail.com>
2026-03-08 12:03:22 +0800

    refactored auth.ts:change type of user props from User to UserResponse

--- cab-booking-frontend/src/types/auth.ts
+++ cab-booking-frontend/src/types/auth.ts
@@ -1,14 +1,15 @@
-import {type User} from './api';
+import { type UserResponse } from './api';
 
 // Shared fields between both roles
 export interface BaseSignupRequest {
+    name: string;
     email: string;
     password: string;
     phoneNumber: string;
 }
 
 // Passenger specific
-export interface PassengerSignupRequest extends BaseSignupRequest {}
+export interface PassengerSignupRequest extends BaseSignupRequest { }
 
 export interface DriverSignupRequest extends BaseSignupRequest {
     licenseNumber: string;
@@ -17,5 +18,5 @@ export interface DriverSignupRequest extends BaseSignupRequest {
 export interface AuthResponse {
     accessToken: string;
     refreshToken: string;
-    user: User;
+    user: UserResponse;
 }
